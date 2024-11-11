b28b5556f67a99c2cf0eaa3c5a9e32b9d2c09b09
FedeBrus <federico.brusadin@gmail.com>
2024-11-11 15:57:39 +0100

    added more functions

--- laboratorio/Collezione/lista.cpp
+++ laboratorio/Collezione/lista.cpp
@@ -5,10 +5,6 @@ struct node {
     node* next;
 };
 
-struct head {
-    node* start;
-};
-
 // Convenzioni:
 // n = primo nodo della lista
 // t = nodo parametro per la funzione
@@ -17,22 +13,23 @@ struct head {
 // m = nuovo nodo
 // c = copia temporanea
 
+// utility functions
 bool is_empty(node* n);
-node* create_node(int x);
-node* create_node(int x, node* t);
-
 void print(node* n);
 void print_rec(node* n);
-
 int length(node* n);
 int length_rec(node* n);
 
+// memory managment functinos
+node* create_node(int x); // Creates a node
+node* create_node(int x, node* t); // Creates node after a certain node t
 void deallocate(node*& n);
+void deallocate_circular(node*& n);
 void deallocate_rec(node*& n);
 
-void insert_at_node(node* n, node* t);
-void insert_at(node*& n, int x, int pos);
-
+// insertion functions
+void insert_at_node(node* n, node* t); // Inserts a certain node t after a certain node n
+void insert_at(node*& n, int x, int pos); // Inserts a node at a certain position
 void add_tail(node*& n, int x);
 void add_head(node*& n, int x);
 void add_node_tail(node*& n, node* t);
@@ -40,32 +37,47 @@ void add_node_head(node*& n, node* t);
 void add_ordered_asc(node*& n, int x);
 void add_ordered_desc(node*& n, int x);
 
+// removal functions
 void remove_head(node*& n);
 void remove_tail(node*& n);
 void remove(node*&, int x);
 void remove_at(node*&, int pos);
 void remove_at_node(node* n);
 
+// access functions 
+node* get_tail(node* n);
+node* get_at(node* n, int pos);
+node* get_max(node* n);
+int get_max_pos(node* n);
+node* get_min(node* n);
+int get_min_pos(node* n);
+
+
+// miscellanious functions
+bool contains(node* n, int x);
+int* get_array(node* n, int& len);
+void invert(node*& n);
+void concatenate(node* n, node* t);
+void make_circular(node* n);
+bool is_circular(node* n);
+void sort(node*& n);
+node* get_list(int arr[], int n);
+
 int main() {
     node* lista = nullptr;
 
+    add_tail(lista, 3);
     add_tail(lista, 1);
     add_tail(lista, 2);
-    add_tail(lista, 3);
-    add_tail(lista, 5);
-    add_tail(lista, 6);
+    add_tail(lista, 7);
+    add_tail(lista, 0);
+    add_tail(lista, 2);
 
     print(lista);
-    remove_at(lista, -2);
-    print(lista);
-    remove_at(lista, 0);
-    print(lista);
-    remove_at(lista, 3);
-    print(lista);
-    remove_at(lista, 2);
+    sort(lista);
     print(lista);
 
-    deallocate(lista);
+    deallocate_circular(lista);
     return 0;
 }
 
@@ -134,6 +146,20 @@ void deallocate(node*& n) {
             n = n->next;
             delete c;
         }
+    }
+}
+
+void deallocate_circular(node*& n) {
+    if (!is_empty(n)) {
+        node* c = n;
+        while (c != n) {
+            node* t = c;
+            c = c->next;
+            delete t;
+        }
+        
+        delete c;
+        n = nullptr;
     }
 }
 
@@ -329,4 +355,191 @@ void remove_at_node(node* n) {
         n->next = c->next;
         delete c;
     }
+}
+
+node* get_tail(node* n) {
+    while (n->next != nullptr) {
+        n = n->next;
+    }
+
+    return n;
+}
+
+node* get_at(node* n, int pos) {
+    if (!is_empty(n)) {
+        for (int i = 0; n->next != nullptr && i < pos; i++) {
+            n = n->next;
+        }
+    }
+
+    return n;
+}
+
+node* get_max(node* n) {
+    node* max_node = n;
+
+    if (!is_empty(n)) {
+        int max = n->val;
+
+        while (n != nullptr) {
+            if (n->val > max) {
+                max_node = n;
+                max = n->val;
+            }
+
+            n = n->next;
+        }
+    }
+
+    return max_node;
+}
+
+int get_max_pos(node* n) {
+    int max_pos = -1;
+
+    int i = 0;
+    if (!is_empty(n)) {
+        max_pos = i;
+        int max = n->val;
+
+        while (n != nullptr) {
+            if (n->val > max) {
+                max_pos = i;
+                max = n->val;
+            }
+
+            n = n->next;
+            i++;
+        }
+    }
+
+    return max_pos;
+}
+
+node* get_min(node* n) {
+    node* min_node = nullptr;
+
+    if (!is_empty(n)) {
+        min_node = n;
+        int min = n->val;
+
+        while (n != nullptr) {
+            if (n->val < min) {
+                min_node = n;
+                min = n->val;
+            }
+
+            n = n->next;
+        }
+    }
+
+    return min_node;
+}
+
+int get_min_pos(node* n) {
+    int min_pos = -1;
+
+    int i = 0;
+    if (!is_empty(n)) {
+        min_pos = i;
+        int min = n->val;
+
+        while (n != nullptr) {
+            if (n->val < min) {
+                min_pos = i;
+                min = n->val;
+            }
+
+            n = n->next;
+            i++;
+        }
+    }
+
+    return min_pos;
+}
+
+bool contains(node* n, int x) {
+    bool found = false;
+
+    if (!is_empty(n)) {
+        while (!found && n != nullptr) {
+            if (n->val == x) found = true;
+            n = n->next;
+        }
+    }
+
+    return found;
+}
+
+int* get_array(node* n, int& len) {
+    len = length(n);
+    int* arr = new int[len];
+    int i = 0;
+    while (i < len && n != nullptr) {
+        arr[i]= n->val;
+        n = n->next;
+        i++;
+    }
+
+    return arr;
+}
+
+void invert(node*& n) {
+    node* t;
+    node* y = n;
+    node* r = nullptr;
+    while (y != nullptr) {
+        t = y->next;
+        y->next = r;
+        r = y;
+        y = t;
+    }
+
+    n = r;
+}
+
+void concatenate(node* n, node* t) {
+    get_tail(n)->next = t;
+}
+
+void make_circular(node* n) {
+    get_tail(n)->next = n;
+}
+
+bool is_circular(node* n) {
+    bool circular = false; 
+    if (!is_empty(n)) {
+        node* c = n;
+        while (c->next != nullptr && c->next != n) {
+            c = c->next;
+        }
+
+        circular = c->next == n;
+    }
+
+    return circular;
+}
+
+void sort(node*& n) {
+    // Very inefficient but works
+    node* sorted = nullptr;
+
+    while (n != nullptr) {
+        node* max = get_max(n);
+        int max_pos = get_max_pos(n);
+        add_head(sorted, max->val);
+        remove_at(n, max_pos);
+    }
+
+    n = sorted;
+}
+
+node* get_list(int arr[], int n) {
+    node* list = nullptr;
+    // Reversed loop for O(n) complexity
+    for (int i = n - 1; i >= 0; i--) {
+        add_head(list, arr[i]);
+    }
+
+    return list;
 }
